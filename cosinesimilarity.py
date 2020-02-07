1b31e5d7a190924d394aad95c903e5e3e0d68783
Utkarsh Chaurasia <msdianuc07@gmail.com>
2020-02-07 14:58:09 +0530

    Update cosinesimilarity.py

--- cosinesimilarity.py
+++ cosinesimilarity.py
@@ -2,7 +2,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 text = ["London Paris London","Paris Paris London"]
-cv = CountVectorizer()
+cv = CountVectorizer() #To compute the cosine similarity. The output of this comes as a sparse_matrix.
 
 count_matrix = cv.fit_transform(text)
 
